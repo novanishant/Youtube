d6db9be10972c2d2850d0a88d7b7921cb343789a
novanishant <nishantverma3633@gmail.com>
2024-01-17 23:30:44 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -16,4 +16,10 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
 app.use(cookieParser());
+
+// router import
+import userRouter from "./routes/user.route.js";
+
+// router declaration
+app.use("/api/v1/users", userRouter);
 export { app };
